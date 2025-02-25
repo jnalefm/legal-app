ac03d6a730d8ba6230502a023a9f6175dd07007a
jnalefm <jnale@forbesmarshall.com>
2025-02-25 15:49:38 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,3 +1,4 @@
 # all llm api keys
 
-GOOGLE_API_KEY = "AIzaSyCcP04LwDVNHvMJMLAKRLwaYf_gmAX9Zdw"
+# GOOGLE_API_KEY = "AIzaSyCcP04LwDVNHvMJMLAKRLwaYf_gmAX9Zdw" unpaid
+GOOGLE_API_KEY = "AIzaSyBem2mE6Xsak76HeWy437FNstXYcIa_kRA" #paid
